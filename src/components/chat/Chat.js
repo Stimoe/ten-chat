926cab5f871a1c06f1914016c05a977d58a450bb
Stimoe <stimoe3@gmail.com>
2020-04-09 16:11:30 -0700

    changed server

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -11,7 +11,7 @@ class Chat extends React.Component{
             messages: []
         };
 
-        this.socket = io('localhost:8080');
+        this.socket = io('https://ten-chat2.herokuapp.com');
 
         this.socket.on('RECEIVE_MESSAGE', function(data){
             addMessage(data);
