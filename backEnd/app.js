625c7991450044ca9d51eb73d969a96b3df6ad82
Stimoe <stimoe3@gmail.com>
2020-04-09 16:48:49 -0700

    testing

--- backEnd/app.js
+++ backEnd/app.js
@@ -14,15 +14,15 @@ var app = express();
 //     console.log('server is running on port 8080')
 // });
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static('client/build'));
   
-    const path = require('path');
-    app.get('*', (req,res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
+//     const path = require('path');
+//     app.get('*', (req,res) => {
+//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+//     })
   
-  }
+//   }
   
   const PORT = process.env.PORT || 5000;
   server = app.listen(PORT, () => {
