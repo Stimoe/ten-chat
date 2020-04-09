27f90519c925571d7cb732d49eb2a7cc5354f808
Stimoe <stimoe3@gmail.com>
2020-04-09 16:05:09 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Chat from "./components/chat/Chat";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+          <Chat/>
+      </div>
+    );
+  }
+}
+
+export default App;
