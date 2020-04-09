46d6f91ff033cded2ee51e28f3ec7065de2daa2f
Stimoe <stimoe3@gmail.com>
2020-04-09 16:31:16 -0700

    changed server

--- backEnd/app.js
+++ backEnd/app.js
@@ -1,12 +1,34 @@
 var express = require('express');
 var socket = require('socket.io');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 var app = express();
 
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGODB_URI || `mongodb://localhost:27017/ten`);
 
-server = app.listen(8080, function(){
-    console.log('server is running on port 8080')
-});
+app.use(bodyParser.json());
+
+// server = app.listen(8080, function(){
+//     console.log('server is running on port 8080')
+// });
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+  
+    const path = require('path');
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+  
+  }
+  
+  const PORT = process.env.PORT || 5000;
+  server = app.listen(PORT, () => {
+    console.log(`app running on port ${PORT}`)
+  });
+
 
 io = socket(server);
 
@@ -17,3 +39,6 @@ io.on('connection', (socket) => {
         io.emit('RECEIVE_MESSAGE', data);
     })
 });
+
+
+
